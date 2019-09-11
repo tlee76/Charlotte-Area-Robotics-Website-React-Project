ad4dcda5f5fd5eec1808d31b986add399072f314
mattwells19 <mwells21@uncc.edu>
2019-09-11 12:21:48 -0400

    Added pages to NavBar

--- src/Main/Main.tsx
+++ src/Main/Main.tsx
@@ -5,6 +5,9 @@ import About from "../About/About";
 import Outreach from "../Outreach/Outreach";
 import { AppBar, Toolbar } from "@material-ui/core"
 import "./Main.scss";
+import Sponsors from "../Sponsors/Sponsors";
+import Competition from "../Competition/Competition";
+import Archive from "../Archive/Archive";
 
 class Main extends Component {
   render() {
@@ -14,13 +17,19 @@ class Main extends Component {
           <Toolbar className="header">
             <NavLink to="/" className="NavBarSelection">Home</NavLink>
             <NavLink to="/about" className="NavBarSelection">About</NavLink>
+            <NavLink to="/sponsors" className="NavBarSelection">Sponsors</NavLink>
             <NavLink to="/outreach" className="NavBarSelection">Outreach</NavLink>
+            <NavLink to="/competition" className="NavBarSelection">Competition</NavLink>
+            <NavLink to="/archive" className="NavBarSelection">Archive</NavLink>
           </Toolbar>
         </AppBar>
         <div className="content">
           <Route exact path="/" component={Home} />
           <Route path="/about" component={About} />
           <Route path="/outreach" component={Outreach} />
+          <Route path="/sponsors" component={Sponsors} />
+          <Route path="/competition" component={Competition} />
+          <Route path="/archive" component={Archive} />
         </div>
       </HashRouter>
     );
