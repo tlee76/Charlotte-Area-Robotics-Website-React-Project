bb00388f6a25cef9112038be05a30889842a7f8f
Matt <mwells21@uncc.edu>
2019-09-11 17:02:46 -0400

    Make NavBar look pretty

--- src/Main/Main.tsx
+++ src/Main/Main.tsx
@@ -1,27 +1,37 @@
 import React, { Component } from "react";
-import { Route, NavLink, HashRouter } from "react-router-dom";
+import { Route, Link, BrowserRouter } from "react-router-dom";
 import Home from "../Home/Home";
 import About from "../About/About";
 import Outreach from "../Outreach/Outreach";
-import { AppBar, Toolbar } from "@material-ui/core"
+import { AppBar, Tabs, Tab } from "@material-ui/core"
 import "./Main.scss";
 import Sponsors from "../Sponsors/Sponsors";
 import Competition from "../Competition/Competition";
 import Archive from "../Archive/Archive";
+import MainLogo from "../Images/CARLogoPrimary.png";
 
 class Main extends Component {
+
+  state = {
+    value: 1,
+  };
+
+  handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+    this.setState({value: newValue});
+  };
+  
   render() {
     return (
-      <HashRouter>
-        <AppBar position="static">
-          <Toolbar className="header">
-            <NavLink to="/" className="NavBarSelection">Home</NavLink>
-            <NavLink to="/about" className="NavBarSelection">About</NavLink>
-            <NavLink to="/sponsors" className="NavBarSelection">Sponsors</NavLink>
-            <NavLink to="/outreach" className="NavBarSelection">Outreach</NavLink>
-            <NavLink to="/competition" className="NavBarSelection">Competition</NavLink>
-            <NavLink to="/archive" className="NavBarSelection">Archive</NavLink>
-          </Toolbar>
+      <BrowserRouter>
+        <AppBar position="static" className="NavBar" style={{backgroundColor: "#212b31"}}>
+          <Tabs centered value={this.state.value} onChange={this.handleChange}>
+            <img src={MainLogo} width="45" height="45" alt="logo"/>
+            <Tab className="NavBarSelection" label="Home" component={Link} to="/"/>
+            <Tab className="NavBarSelection" label="About" component={Link} to="/about"/>
+            <Tab className="NavBarSelection" label="Sponsors" component={Link} to="/sponsors"/>
+            <Tab className="NavBarSelection" label="Competition" component={Link} to="/competition"/>
+            <Tab className="NavBarSelection" label="Archive" component={Link} to="/archive"/>
+          </Tabs>
         </AppBar>
         <div className="content">
           <Route exact path="/" component={Home} />
@@ -31,7 +41,7 @@ class Main extends Component {
           <Route path="/competition" component={Competition} />
           <Route path="/archive" component={Archive} />
         </div>
-      </HashRouter>
+      </BrowserRouter>
     );
   }
 }
