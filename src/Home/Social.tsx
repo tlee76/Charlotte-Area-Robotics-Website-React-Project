05a5138fc17ab2e72546237bb5828cc82d30756a
Matt <mwells21@uncc.edu>
2019-09-30 19:49:22 -0400

    Added PayPal button (#3)

--- src/Home/Social.tsx
+++ src/Home/Social.tsx
@@ -6,8 +6,10 @@ import EasyToSeeTooltip from "../Main/EasyToSeeTooltip";
 export default function Social(props: any) {
     const { classes } = props;
     return (
-        <Typography variant="h4">
-            Follow us on social media!
+        <div>
+            <Typography variant="h4">
+                Support us on Social Media or Donate!
+            </Typography>
             <Divider className={classes.divider} />
             <EasyToSeeTooltip title="https://www.instagram.com/car_robotics/">
                 <IconButton href="https://www.instagram.com/car_robotics/">
@@ -19,6 +21,14 @@ export default function Social(props: any) {
                     <Facebook fontSize="large" color="action" />
                 </IconButton>
             </EasyToSeeTooltip>
-        </Typography>
+            <EasyToSeeTooltip title="PayPal - The safer, easier way to pay online!">
+                <form action="https://www.paypal.com/cgi-bin/webscr" target="_blank" method="post">
+                    <input type="hidden" name="cmd" value="_s-xclick" />
+                    <input type="hidden" name="hosted_button_id" value="FBMKDCDK2PUXN" />
+                    <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" name="submit" alt="Donate with PayPal button" />
+                    <img alt="" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+                </form>
+            </EasyToSeeTooltip>
+        </div>
     );
 }
